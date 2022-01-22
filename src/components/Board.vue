6b7b7ad7f32ef3edac62e9b8d3e79987d088ba74
TakuroKawakami <Takur0.o722.job@gmail.com>
2022-01-22 17:09:40 +0900

    ドラッグ&ドロップ機能を追加しました

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -4,31 +4,50 @@
       my Trello
     </header>
     <main>
-      <p class="info-line">All: 0 tasks</p>
-      <list v-for="(item, index) in lists"
-            :key="item.id"
-            :title="item.title"
-            :listIndex="index"
-      />
-      <list-add/>
+      <p class="info-line">All: {{ totalCardCount }} tasks</p>
+      <div class="list-index">
+      <draggable :list="lists"
+                  @end="movingList"
+                  class="list-index">
+        <List v-for="(item, index) in lists"
+              :key="item.id"
+              :title="item.title"
+              :cards="item.cards"
+              :listIndex="index"
+              @change="movingCard"
+        />
+        <ListAdd />
+      </draggable>
+      </div>
     </main>
   </div>
 </template>
-
 <script>
+import draggable from 'vuedraggable'
+import ListAdd from './ListAdd.vue'
 import List from './List'
-import ListAdd from './ListAdd'
 import { mapState } from 'vuex'
-
 export default {
   components: {
     ListAdd,
     List,
+    draggable
   },
   computed: {
     ...mapState([
       'lists'
     ]),
+    totalCardCount() {
+      return this.$store.getters.totalCardCount
+    }
   },
+  methods: {
+    movingCard: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    },
+    movingList: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    }
+  }
 }
 </script>
