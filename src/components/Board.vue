25dc0c84cbde097cfbe9f555ae33f58bf5065b9d
TakuroKawakami <Takur0.o722.job@gmail.com>
2022-01-22 16:00:37 +0900

    ローカルストレージに保存する途中です

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,16 +1,20 @@
 <template>
-  <div>
-    <header>
-      my Trello
-    </header>
-    <main>
+    <div>
+      <header>
+        my Trello
+      </header>
+      <main>
       <p class="info-line">All: 0 tasks</p>
+      <list-add />
     </main>
   </div>
 </template>
 
 <script>
- export default {
-
- }
+import ListAdd from'./ListAdd.vue'
+  export default {
+    components: {
+      ListAdd
+    },
+  }
 </script>
